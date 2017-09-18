8fc6d175a53c9ba32f002a0ecebf778674ac3c19
Heinoc <tracychsh@126.com>
2017-09-18 11:31:10 +0800

    替换nginx为Tengine;

--- Nginx/run.sh
+++ Nginx/run.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
 
-# 启动 supervisord
-service supervisord start
-# 启动 sshd
-service sshd start
 # 启动 nginx
-/home/work/_app/nginx/sbin/nginx
+/home/work/app/nginx/sbin/nginx
 
 # outpu log
-tail -F /home/work/_logs/supervisord/supervisord.log \
-		/home/work/_logs/nginx/default.access.log \
-		/home/work/_logs/nginx/default.error.log \
-		/home/work/_logs/nginx/error.log
+#tail -F /home/work/logs/supervisord/supervisord.log \
+#		/home/work/logs/nginx/default.access.log \
+#		/home/work/logs/nginx/default.error.log \
+#		/home/work/logs/nginx/error.log
